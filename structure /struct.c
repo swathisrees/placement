10e75259fe748e1a2f12fbad1dab3c80a4dd0822
swathisrees <70190980+swathisrees@users.noreply.github.com>
2023-08-16 10:40:33 +0000

    updated

---
+++ structure /struct.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+struct empdetails
+{
+    int id;
+    char name [1024];
+    float salary;
+    int phoneno;
+    char gender;
+};
+
+int main()
+{
+    struct empdetails e;
+    printf("ENTER THE NUMBER OF EMPLOYES");
+    scanf("")
+
+}
