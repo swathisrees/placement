3490b73fff8a0f2dc10a3264b5b1641e5aa00424
swathisrees <70190980+swathisrees@users.noreply.github.com>
2023-08-16 08:01:47 +0000

    updated

---
+++ strings/rotastr.c
@@ -0,0 +1,10 @@
+/*rotate string after every 'k'th value*/
+
+
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    
+}
---
+++ strings/wordrev.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverseWords(char *str) {
+    int length = strlen(str);
+    int start = 0;
+
+    for (int i = 0; i < length; i++) {
+        if (str[i] == '.' || i == length - 1) {
+            int end = (i == length - 1) ? i : i - 1;
+
+            while (start < end) {
+                char temp = str[start];
+                str[start] = str[end];
+                str[end] = temp;
+                start++;
+                end--;
+            }
+
+            start = i + 1;
+        }
+    }
+
+    // Reverse the entire string
+    int fullEnd = length - 1;
+    while (start < fullEnd) {
+        char temp = str[start];
+        str[start] = str[fullEnd];
+        str[fullEnd] = temp;
+        start++;
+        fullEnd--;
+    }
+}
+
+int main() {
+    char input[1000];
+    printf("Input: ");
+    scanf("%[^\n]", input);
+
+    reverseWords(input);
+
+    printf("Output: %s\n", input);
+
+    return 0;
+}
